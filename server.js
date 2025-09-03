52ae9dacf2ff09742a3a982e20a747402f1c4146
Akhil Goyal <108260649+Akhil090702@users.noreply.github.com>
2025-09-03 18:51:58 +0530

    Changes made for deployment

--- server.js
+++ server.js
@@ -1,27 +1,31 @@
-import dotenv from "dotenv";
-import express from "express";
-import mongoose from "mongoose";
-import cors from "cors";
-import movieRoutes from "./routes/movieRoutes.js";
+// import dotenv from "dotenv";
+// import express from "express";
+// import mongoose from "mongoose";
+// import cors from "cors";
+// import movieRoutes from "./routes/movieRoutes.js";
 
-dotenv.config();
+// dotenv.config();
 
-const app = express();
-const PORT = process.env.PORT || 2000;
+// const app = express();
+// const PORT = process.env.PORT || 2000;
 
-app.use(cors());
-app.use(express.json());
+// app.use(cors());
+// app.use(express.json());
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.error("MongoDB connection error:", err));
+// mongoose
+//   .connect(process.env.MONGO_URI)
+//   .then(() => console.log("MongoDB Connected"))
+//   .catch((err) => console.error("MongoDB connection error:", err));
 
-app.use("/movies", movieRoutes);
-// app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+// app.use("/movies", movieRoutes);
+// // app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 
-app.get("/", (req, res) => {
-  res.send("Movie. are visible on vercel");
-});
+// app.get("/", (req, res) => {
+//   res.send("Movie. are visible on vercel");
+// });
 
-export default app;
+// export default app;
+
+// server.js
+import app from "./App.js";
+export default app; // Vercel will use this
